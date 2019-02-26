55d33bbc9c2bd5ba33233144fd258b22e4d8b7d6
xXmemestarXx <matrix.anonymous123@gmail.com>
2019-02-26 06:02:58 +0100

    Added Clock

--- Website/JS/main.js
+++ Website/JS/main.js
@@ -1,3 +1,4 @@
+//Opinion handling
 function opinion(){
     var input = document.getElementById('opinion').value;
     if(input.length > 6){
@@ -5,4 +6,23 @@ function opinion(){
     } else if(input.length < 20){
         alert("That is not over 20 characters");
     }
+}
+//Clock
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =
+        h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i
+    }; // add zero in front of numbers < 10
+    return i;
 }
